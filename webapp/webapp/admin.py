de876d279c4e6e8387b74730fcbf1e823855b583
amrutha.k <ammukunati.com>
2020-12-19 12:31:13 +0530

    backend

--- webapp/webapp/admin.py
+++ webapp/webapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import product
+from .models import product,seller,buyer,cart,orders,address
 # Register your models here.
 
 admin.site.register(product)
-# admin.site.register(order)
-# admin.site.register(buyer)
+admin.site.register(seller)
+admin.site.register(buyer)
+admin.site.register(cart)
+admin.site.register(orders)
+admin.site.register(address)
